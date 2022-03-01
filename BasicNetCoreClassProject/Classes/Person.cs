ca7242a6a041b6fd20fcb17cd58862cd2d104ebb
AmeliaD2021 <90657892+AmeliaD2021@users.noreply.github.com>
2022-03-01 13:30:41 -0800

    Add project files.

---
+++ BasicNetCoreClassProject/Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicNetCoreClassProject.Interfaces;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BasicNetCoreClassProject.Classes
+{
+    public class Person : IBase
+    {
+        public int Identifier { get; set; }
+        public int Id => Identifier;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() => $"{Id,-5}{FirstName} {LastName}";
+    }
+
+}
---
+++ BasicNetCoreClassProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicNetCoreClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
