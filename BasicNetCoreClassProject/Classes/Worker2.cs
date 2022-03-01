fab93cb451d2a9caeb7c6fcf54ae427970422fe9
AmeliaD2021 <90657892+AmeliaD2021@users.noreply.github.com>
2022-03-01 15:41:00 -0800

    added Mocked and Worker2 classes

---
+++ BasicNetCoreClassProject/Classes/Worker2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicNetCoreClassProject.Interfaces;
+
+namespace BasicNetCoreClassProject.Classes
+{
+    public class Worker2
+    {
+
+        public static void CompareValue<T>(List<T> sender) where T : class, IBase
+        {
+            foreach (var item in sender)
+            {
+                Debug.WriteLine(item.Id);
+            }
+        }
+
+    }
+
+}
+
